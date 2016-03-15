fcf34f4d81105f4a46c1ad8357016c1b3c047b7d
Yves Brissaud <yves.brissaud@gmail.com>
2016-03-15 21:24:48 +0100

    Add demo of elm script with sprockets

---
+++ app/assets/javascripts/StringUtils.elm
@@ -0,0 +1,4 @@
+module StringUtils where
+
+concat2 : String -> String -> String
+concat2 str1 str2 = str1 ++ " " ++ str2
---
+++ app/assets/javascripts/welcome.js.elm
@@ -0,0 +1,6 @@
+import Html exposing (text)
+import StringUtils as Utils
+
+main =
+  text (Utils.concat2 "Hello" "World")
+
